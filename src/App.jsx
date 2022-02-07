dbf2c0a00c6f1077973a4d7979fad84154a3f091
Roberto <maisto_roberto@hotmail.it>
2022-02-07 15:48:21 +0100

    adds functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import GreetingOutput from './GreetingOutput';
+
 
 export class App extends Component {
+  // constructor(props) {
+     // super(props)
+      //this.state = {
+      //  planet: 'Venus'
+      //}
+    //}
+  state = {
+    planet: "Venus",
+  };
+
   render() {
-    return <div>
-      <h1>Hello world</h1>
-    </div>;
-  }
+    // const planet = "Venus";
+    const { planet } = this.state;
+    return (
+      <div>
+        <GreetingOutput planet={planet} />
+        <input type="text" placeholder="Enter planet name"
+          onChange={(event) => {
+            this.setState({ planet: event.target.value });
+          }}
+        />
+    </div>
+    );
 }
+}
+
 
 export default App;
